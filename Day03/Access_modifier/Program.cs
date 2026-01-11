2c99f6d8abd38b95445e697cec23a5ed79cc0f75
nitika2717 <nitikagrover.2717@gmail.com>
2026-01-11 21:40:07 +0530

    Add files via upload

---
+++ Day03/Access_modifier/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+// BANK ACCOUNT SYSTEM
+public class BankAccount
+{
+    // private:
+    // PIN is sensitive data
+    // Accessible ONLY inside this class
+    private int pin = 1234;
+
+    // public:
+    // Account number should be accessible
+    // by external applications, users, APIs
+    public string AccountNumber = "ACC1001";
+
+    // protected:
+    // Interest calculation logic
+    // Can be used ONLY by child (derived) account classes
+    // Example: SavingAccount, CurrentAccount
+    protected double CalculateInterest(double balance)
+    {
+        return balance * 0.05;
+    }
+
+    // internal:
+    // Audit logic should work only
+    // inside the same bank project (assembly)
+    // Not accessible to outside applications
+    internal void AuditTransaction()
+    {
+        Console.WriteLine("Transaction audited");
+    }
+
+    // protected internal:
+    // Can be accessed:
+    // 1. Anywhere inside same project
+    // 2. Derived classes from another project (partner banks)
+    protected internal void PartnerBankFeature()
+    {
+        Console.WriteLine("Partner bank feature accessed");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        BankAccount account = new BankAccount();
+
+        Console.WriteLine(account.AccountNumber); //  public
+        account.AuditTransaction();               //  internal
+        account.PartnerBankFeature();             //  protected internal
+
+        // account.pin  NOT accessible (private)
+        // account.CalculateInterest(1000) NOT accessible (protected)
+    }
+}
+
---
+++ Day03/abstract_class/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+abstract class Product
+{
+    public int ProductID { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+
+    // Abstract method (NO BODY)
+    public abstract void DisplayProductNameDetail();
+
+    // Must be PUBLIC
+    public void DisplayProductDetail()
+    {
+        Console.WriteLine("Product Name: " + ProductName);
+        Console.WriteLine("Product ID: " + ProductID);
+    }
+}
+
+class ElectronicProduct : Product  // derived class ElectronicProduct inherits Product
+
+// Gains:
+
+// ProductID
+
+// ProductName
+
+// DisplayProductDetail()
+/{
+    public string Brand { get; set; } = string.Empty;
+
+    public override void DisplayProductNameDetail() //Because this method is abstract in parent
+    {
+        Console.WriteLine("Electronic Product Name: " + ProductName);
+        Console.WriteLine("Brand: " + Brand);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ElectronicProduct ep = new ElectronicProduct();
+        ep.ProductID = 101;
+        ep.ProductName = "Laptop";
+        ep.Brand = "Dell";
+
+        ep.DisplayProductDetail();
+        ep.DisplayProductNameDetail();
+    }
+}
