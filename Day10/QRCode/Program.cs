233693f0bc9abedb7ebb11a89ac0d32cb981ebac
nitika2717 <nitikagrover.2717@gmail.com>
2026-01-21 22:16:47 +0530

    Add files via upload

---
+++ Day10/QRCode/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using QRCoder;
+
+string data = "https://www.google.com";
+
+QRCodeGenerator qrGenerator = new QRCodeGenerator();
+QRCodeData qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+
+PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+byte[] qrCodeBytes = qrCode.GetGraphic(20);
+
+// Save QR code as image
+File.WriteAllBytes("qrcode.png", qrCodeBytes);
+
+Console.WriteLine("QR Code generated successfully!");
+
