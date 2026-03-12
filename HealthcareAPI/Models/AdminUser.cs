a5cd7bfe355a14790738ddbcfa1624ea73b84818
nitika2717 <nitikagrover.2717@gmail.com>
2026-03-12 16:20:37 +0530

    Delete HealthcareAPI directory

--- HealthcareAPI/Models/AdminUser.cs
+++
@@ -1,9 +0,0 @@
-namespace HealthcareAPI.Models
-{
-    public class AdminUser
-    {
-        public int AdminUserId { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-    }
-}
