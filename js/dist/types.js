32961db13ea0a0310189961b6cbfa7bdb8b3785b
nitika2717 <nitikagrover.2717@gmail.com>
2026-03-12 16:21:15 +0530

    Delete js directory

--- js/dist/types.js
+++
@@ -1,7 +0,0 @@
-// TypeScript types and enums
-export var AppointmentStatus;
-(function (AppointmentStatus) {
-    AppointmentStatus[AppointmentStatus["Scheduled"] = 0] = "Scheduled";
-    AppointmentStatus[AppointmentStatus["Completed"] = 1] = "Completed";
-    AppointmentStatus[AppointmentStatus["Cancelled"] = 2] = "Cancelled";
-})(AppointmentStatus || (AppointmentStatus = {}));
